83de92acaa0f0fa8379fa735b450a2e2181a8bfe
caoyabei <1039769138@qq.com>
2019-05-07 22:17:00 +0800

    zuijin

--- imoocMall/src/main.js
+++ imoocMall/src/main.js
@@ -3,9 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLazyload from 'vue-lazyload'
+import infiniteScroll from  'vue-infinite-scroll'
 import {sum,minus} from './util'
 Vue.config.productionTip = false
-
+Vue.use(infiniteScroll);
+Vue.use(VueLazyload, {
+  loading: '/static/loading-svg/loading-bars.svg',
+  try: 3 // default 1
+})
+import './assets/css/base.css'
+import './assets/css/checkout.css'
+import './assets/css/product.css'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
